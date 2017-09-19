9c19ae856e2770ada4f8b4dec10419f4427b863b
BramBear <alvin.reyes@botmill.io>
2017-09-18 20:34:21 -0400

    Added BlockApi blocktype parameter
    Make BlockTypes.REGULAR as default deserialization

--- src/main/java/io/nem/apps/api/BlockApi.java
+++ src/main/java/io/nem/apps/api/BlockApi.java
@@ -33,15 +33,16 @@ public class BlockApi {
 					fn.printStackTrace();
 					return null;
 				}).get();
-		return new Block(BlockTypes.NEMESIS, DeserializationOptions.NON_VERIFIABLE, des);
+		return new Block(BlockTypes.REGULAR, DeserializationOptions.NON_VERIFIABLE, des);
 	}
 
 	/**
 	 * Gets the block after given block height.
 	 *
-	 * @param height
-	 *            the height
+	 * @param height            the height
 	 * @return the block after given block height
+	 * @throws InterruptedException the interrupted exception
+	 * @throws ExecutionException the execution exception
 	 */
 	public static Block getBlockAfterGivenBlockHeight(int height) throws InterruptedException, ExecutionException {
 		Deserializer des;
@@ -53,6 +54,49 @@ public class BlockApi {
 					return null;
 				}).get();
 		
-		return new Block(BlockTypes.NEMESIS, DeserializationOptions.NON_VERIFIABLE, des);
+		return new Block(BlockTypes.REGULAR, DeserializationOptions.NON_VERIFIABLE, des);
+	}
+	
+	/**
+	 * Gets the block by height and blocktype
+	 *
+	 * @param height the height
+	 * @param blockType the block type
+	 * @return the block by height
+	 * @throws InterruptedException the interrupted exception
+	 * @throws ExecutionException the execution exception
+	 */
+	public static Block getBlockByHeight(int height, int blockType) throws InterruptedException, ExecutionException {
+		Deserializer des;
+		JSONObject jsonHeight = new JSONObject();
+		jsonHeight.put("height", height);
+		des = Globals.CONNECTOR.postAsync(Globals.getNodeEndpoint(), NisApiId.NIS_REST_BLOCK_AT_PUBLIC,
+				new HttpJsonPostRequest(jsonHeight)).exceptionally(fn -> {
+					fn.printStackTrace();
+					return null;
+				}).get();
+		return new Block(blockType, DeserializationOptions.NON_VERIFIABLE, des);
+	}
+	
+	/**
+	 * Gets the block after given block height and blocktype
+	 *
+	 * @param height the height
+	 * @param blockType the block type
+	 * @return the block after given block height
+	 * @throws InterruptedException the interrupted exception
+	 * @throws ExecutionException the execution exception
+	 */
+	public static Block getBlockAfterGivenBlockHeight(int height,int blockType) throws InterruptedException, ExecutionException {
+		Deserializer des;
+		JSONObject jsonHeight = new JSONObject();
+		jsonHeight.put("height", height);
+		des = Globals.CONNECTOR.postAsync(Globals.getNodeEndpoint(), NisApiId.NIS_REST_BLOCK_AFTER_LOCAL,
+				new HttpJsonPostRequest(jsonHeight)).exceptionally(fn -> {
+					fn.printStackTrace();
+					return null;
+				}).get();
+		
+		return new Block(blockType, DeserializationOptions.NON_VERIFIABLE, des);
 	}
 }
