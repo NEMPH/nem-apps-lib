f83a0a2411a25caacaa1f930db5157985f2de9d7
--add <alvin.reyes@botmill.io>
2018-03-06 21:10:38 -0500

    Fixed the globals

--- src/main/java/io/nem/apps/service/NemAppsLibGlobals.java
+++ src/main/java/io/nem/apps/service/NemAppsLibGlobals.java
@@ -8,107 +8,29 @@ import org.nem.core.node.ApiId;
 import org.nem.core.node.NodeEndpoint;
 import org.nem.core.time.SystemTimeProvider;
 import org.nem.core.time.TimeProvider;
-
 import io.nem.apps.factories.ConnectorFactory;
-import io.nem.apps.fee.TransactionFeeCalculatorAfterForkForApp;
-import io.nem.apps.util.AppPropertiesUtil;
-
-
 
 /**
  * The Class Globals.
  */
 public class NemAppsLibGlobals {
 
-	/** The Constant NETWORK_NAME. */
-	public static final String NETWORK_NAME = "mijinnet"; // testnet, mainnet,
-															
-															/** The Constant NETWORK_TYPE. */
-															// mijinnet
-	public static final long NETWORK_TYPE = 2; // 0:testnet, 1:mainnet,
-												
-												/** The Constant NEMSISTIME. */
-												// 2:mijinnet
-	public static final long NEMSISTIME = 1427587585; // the first block time
-	
-	/** The Constant MICRONEMS_IN_NEM. */
-	public static final long MICRONEMS_IN_NEM = 1000000;
-	
-	/** The Constant WEBSOCKET_PORT. */
-	public static final String WEBSOCKET_PORT = "7778";
-
-	/** The Constant URL_INIT_TRANSACTION. */
-	public static final String URL_INIT_TRANSACTION = "/transaction/prepare-announce";
-	
-	/** The Constant URL_ACCOUNT_GET. */
-	public static final String URL_ACCOUNT_GET = "/account/get";
-	
-	/** The Constant URL_ACCOUNT_TRANSFERS_INCOMING. */
-	public static final String URL_ACCOUNT_TRANSFERS_INCOMING = "/account/transfers/incoming";
-	
-	/** The Constant URL_ACCOUNT_TRANSFERS_OUTGOING. */
-	public static final String URL_ACCOUNT_TRANSFERS_OUTGOING = "/account/transfers/outgoing";
-	
-	/** The Constant URL_ACCOUNT_GET_FROMPUBLICKEY. */
-	public static final String URL_ACCOUNT_GET_FROMPUBLICKEY = "/account/get/from-public-key";
-	
-	/** The Constant URL_ACCOUNT_UNCONFIRMEDTRANSACTIONS. */
-	public static final String URL_ACCOUNT_UNCONFIRMEDTRANSACTIONS = "/account/unconfirmedTransactions";
-	
-	/** The Constant URL_ACCOUNT_MOSAIC_OWNED. */
-	public static final String URL_ACCOUNT_MOSAIC_OWNED = "/account/mosaic/owned";
-	
-	/** The Constant URL_TRANSACTION_ANNOUNCE. */
-	public static final String URL_TRANSACTION_ANNOUNCE = "/transaction/announce";
-	
+	public static final String CONTENT_TYPE_TEXT_JSON = "application/json";
 	/** The Constant URL_NAMESPACE_MOSAIC_DEFINITION_PAGE. */
 	public static final String URL_NAMESPACE_MOSAIC_DEFINITION_PAGE = "/namespace/mosaic/definition/page";
-
-	/** The Constant URL_WS_W_MESSAGES. */
-	public static final String URL_WS_W_MESSAGES = "/w/messages";
-	
-	/** The Constant URL_WS_W_API_ACCOUNT_SUBSCRIBE. */
-	public static final String URL_WS_W_API_ACCOUNT_SUBSCRIBE = "/w/api/account/subscribe";
-	
-	/** The Constant URL_WS_TRANSACTIONS. */
-	public static final String URL_WS_TRANSACTIONS = "/transactions";
-	
-	/** The Constant URL_WS_UNCONFIRMED. */
-	public static final String URL_WS_UNCONFIRMED = "/unconfirmed";
-
-	/** The Constant CONTENT_TYPE_TEXT_JSON. */
-	public static final String CONTENT_TYPE_TEXT_JSON = "application/json";
-
-	/** The Constant HELPER_FILE_INIT_TRANSACTION. */
-	public static final String HELPER_FILE_INIT_TRANSACTION = "initTransaction_parameter.txt";
-	
-	/** The Constant HELPER_FILE_INIT_MULTISIG_TRANSACTION. */
-	public static final String HELPER_FILE_INIT_MULTISIG_TRANSACTION = "initMultisigTransaction_parameter.txt";
-	
-	/** The Constant HELPER_FILE_COSIGN_MULTISIG_TRANSACTION. */
-	public static final String HELPER_FILE_COSIGN_MULTISIG_TRANSACTION = "cosignMultisigTransaction_parameter.txt";
-	
-	/** The Constant HELPER_FILE_MONITOR_INCOMING_TRANSACTION. */
-	public static final String HELPER_FILE_MONITOR_INCOMING_TRANSACTION = "monitorIncomingTransaction_parameter.txt";
-	
-	/** The Constant HELPER_FILE_MONITOR_MULTISIG_TRANSACTION. */
-	public static final String HELPER_FILE_MONITOR_MULTISIG_TRANSACTION = "monitorMultisigTransaction_parameter.txt";
-
-	/** The Constant TX_TYPE_INIT_MULTISIG. */
-	public static final long TX_TYPE_INIT_MULTISIG = 4100;
-	
-	/** The Constant TX_TYPE_COSIGN_MULTISIG. */
-	public static final long TX_TYPE_COSIGN_MULTISIG = 4098;
 
 	/** The Constant TIME_PROVIDER. */
 	public static final TimeProvider TIME_PROVIDER = new SystemTimeProvider();
 
 	/** The Constant NODE_ENDPOINT. */
 	private static NodeEndpoint NODE_ENDPOINT;
-	
+
 	/** The fee calculator. */
-	private static TransactionFeeCalculator feeCalculator = new FeeUnitAwareTransactionFeeCalculator(Amount.fromMicroNem(50_000L), null);
-	private static TransactionFeeCalculator feeCalculatorMultiSig = new FeeUnitAwareTransactionFeeCalculator(Amount.fromMicroNem(50_000L), null);
+	private static TransactionFeeCalculator feeCalculator = new FeeUnitAwareTransactionFeeCalculator(
+			Amount.fromMicroNem(50_000L), null);
+	private static TransactionFeeCalculator feeCalculatorMultiSig = new FeeUnitAwareTransactionFeeCalculator(
+			Amount.fromMicroNem(50_000L), null);
+
 	/**
 	 * Gets the node endpoint.
 	 *
@@ -121,12 +43,13 @@ public class NemAppsLibGlobals {
 	/**
 	 * Sets the global transaction fee.
 	 *
-	 * @param feeCalculator the new global transaction fee
+	 * @param feeCalculator
+	 *            the new global transaction fee
 	 */
 	public static void setGlobalTransactionFee(TransactionFeeCalculator feeCalculator) {
 		NemAppsLibGlobals.feeCalculator = feeCalculator;
 	}
-	
+
 	public static void setGlobalMultisigTransactionFee(TransactionFeeCalculator feeCalculator) {
 		NemAppsLibGlobals.feeCalculatorMultiSig = feeCalculator;
 	}
@@ -139,7 +62,7 @@ public class NemAppsLibGlobals {
 	public static TransactionFeeCalculator getGlobalTransactionFee() {
 		return feeCalculator;
 	}
-	
+
 	public static TransactionFeeCalculator getGlobalMultisigTransactionFee() {
 		return feeCalculatorMultiSig;
 	}
@@ -151,7 +74,8 @@ public class NemAppsLibGlobals {
 	/**
 	 * Sets the node endpoint.
 	 *
-	 * @param endpoint the new node endpoint
+	 * @param endpoint
+	 *            the new node endpoint
 	 */
 	public static void setNodeEndpoint(NodeEndpoint endpoint) {
 		NemAppsLibGlobals.NODE_ENDPOINT = endpoint;
